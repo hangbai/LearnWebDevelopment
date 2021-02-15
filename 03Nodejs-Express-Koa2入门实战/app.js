9dae0e3005f5475c5f12a9575aab532b8288ef19
Hang Bai <baihang@gmail.com>
2021-02-15 20:44:08 +0100

    03

--- 03Nodejs-Express-Koa2入门实战/app.js
+++ 03Nodejs-Express-Koa2入门实战/app.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const ejs = require('ejs')
-const app = express()
-app.engine('html',ejs.__express) // 修改ejs 后缀
-app.set('view engine','html') //加载引擎
-app.use(express.static('static'))
-app.get('/home',(req,res)=>{
-  let title = '你好'
-  res.render('index',{title})
-})
-// 监听
-app.listen(8080)
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000)
---
+++ 笔记/koa.md
@@ -0,0 +1,3 @@
+# koa中文教程
+https://www.itying.com/koa/
+
